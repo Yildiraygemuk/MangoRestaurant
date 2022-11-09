4e4854d347866012d7c212819ec76e33abc7e065
Yıldıray Gemük <yildiraygemuk@gmail.com>
2022-11-09 23:38:01 +0300

    Add project files.

---
+++ Mango.Services.API/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.API
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Mango.Services.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.API.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        [Range(1,1000)]
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
