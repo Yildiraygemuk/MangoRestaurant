b8753747aca659d8fdd45789f5b9d03ff5795869
Yıldıray Gemük <yildiraygemuk@gmail.com>
2022-11-15 00:27:53 +0300

    AutoMapper and IProductRepository

--- Mango.Services.API/Program.cs
+++ Mango.Services.API/Program.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Mango.Services.ProductAPI;
+using Mango.Services.ProductAPI.Mapping;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Mango.Services.API/Repository/IProductRepository.cs
+++ Mango.Services.API/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
     }
 }
