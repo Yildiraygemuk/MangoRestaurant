87c1e62fe48b4695e9dec4aa43ef3bad28d3d4a6
Yıldıray Gemük <yildiraygemuk@gmail.com>
2022-11-24 23:49:58 +0300

    Get  & GetById Methods

--- Mango.Services.API/Models/Dto/ResponseDto.cs
+++ Mango.Services.API/Models/Dto/ResponseDto.cs
@@ -2,7 +2,7 @@
 {
     public class ResponseDto
     {
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = true;
         public object Result { get; set; }
         public string DisplayMessage { get; set; } = "";
         public List<string> ErrorMessages { get; set; }
