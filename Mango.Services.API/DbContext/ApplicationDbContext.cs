337fe4ff6a1b1e722765d2bbc0392a6929f21c6b
Yıldıray Gemük <yildiraygemuk@gmail.com>
2022-11-26 20:36:35 +0300

    OnModelCreating Update

--- Mango.Services.API/DbContext/ApplicationDbContext.cs
+++ Mango.Services.API/DbContext/ApplicationDbContext.cs
@@ -10,5 +10,45 @@ namespace Mango.Services.ProductAPI
 
         }
         public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Samosa",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum.",
+                ImageUrl = "",
+                CategoryName = "Appetize"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Paneer Tikka",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum.",
+                ImageUrl = "",
+                CategoryName = "Appetize"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Sweet Pie",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum.",
+                ImageUrl = "",
+                CategoryName = "Appetize"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Pay bhaji",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum.",
+                ImageUrl = "",
+                CategoryName = "Appetize"
+            });
+        }
     }
 }
