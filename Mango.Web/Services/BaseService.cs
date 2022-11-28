1e16390c298aae9d9d80c09a1cb372d67c8cc09d
Yıldıray Gemük <yildiraygemuk@gmail.com>
2022-11-28 22:06:15 +0300

    Base Service Interface and API request model

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get; set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
