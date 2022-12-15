2c9c768d28e63bc9b3c2960c16a4a6dd34a43a81
Yıldıray Gemük <yildiraygemuk@gmail.com>
2022-12-15 22:21:22 +0300

    Configuring Clients in Identity Server

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Mango.Services.Identity
 {
@@ -14,11 +15,36 @@ namespace Mango.Services.Identity
                 new IdentityResources.Profile()
             };
         public static IEnumerable<ApiScope> ApiScopes =>
-            new List<ApiScope> { new ApiScope("Mango","Mango Server"),
+            new List<ApiScope> { new ApiScope("mango","Mango Server"),
                 new ApiScope(name: "read", displayName:"Read your data."),
                 new ApiScope(name: "write", displayName:"Write your data."),
-                new ApiScope(name: "delete", displayName:"Delete your data."),
-
-            }
+                new ApiScope(name: "delete", displayName:"Delete your data.")
+            };
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes={"read","write","profile"}
+                },
+                 new Client
+                {
+                    ClientId="mango",
+                    ClientSecrets={new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris ={ "https://localhost:44321/signin-oidc" },
+                    PostLogoutRedirectUris={ "https://localhost:44321/signout-callback-oidc" },
+                    AllowedScopes=new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "mango"
+                    }
+                }
+            };
     }
 }
